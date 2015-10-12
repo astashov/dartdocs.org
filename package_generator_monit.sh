33f6519f2bbe662622fe74734fc640f8114a9186
Anton Astashov <anton.astashov@gmail.com>
2015-10-11 22:10:58 -0500

    Avoid destroying the log file on process restart

--- package_generator_monit.sh
+++ package_generator_monit.sh
@@ -7,7 +7,7 @@ case $1 in
    start)
       echo $$ > /var/run/package_generator.pid;
       mkdir -p $DIRROOT/logs
-      exec 2>&1 dart $DIRROOT/bin/package_generator.dart --dirroot $DIRROOT 1>$DIRROOT/logs/log.txt
+      exec 2>&1 dart $DIRROOT/bin/package_generator.dart --dirroot $DIRROOT 1>>$DIRROOT/logs/log.txt
       ;;
     stop)
       kill `cat /var/run/package_generator.pid` ;;
