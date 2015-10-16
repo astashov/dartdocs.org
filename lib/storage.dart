2ca7f6d581e0c3837591d28ebd53a63bb3c42646
Anton Astashov <anton.astashov@gmail.com>
2015-10-16 10:13:32 -0500

    Use non-resumable uploads with pre-calculated length to Storage
    By some reason, resumable uploads fail pretty often.
    So, let's try to calculate the length before upload and try regular
    uploads

--- lib/storage.dart
+++ lib/storage.dart
@@ -31,11 +31,11 @@ class Storage {
 
   Future<Null> insertFile(String path, File file, {String contentType, int maxAge: 3600}) async {
     contentType = contentType ?? _getContentType(path);
+    var length = await file.openRead().transform(GZIP.encoder).fold(0, (memo, b) => memo + b.length);
     var stream = file.openRead().transform(GZIP.encoder);
-    var media = new s.Media(stream, null, contentType: contentType);
+    var media = new s.Media(stream, length, contentType: contentType);
     await retry(() async {
       return (await _storageApi).objects.insert(new s.Object.fromJson({"cacheControl": "public, max-age=$maxAge"}), config.bucket,
-          uploadOptions: s.UploadOptions.Resumable,
           contentEncoding: "gzip",
           name: path,
           uploadMedia: media,
