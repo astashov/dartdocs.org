3749a8ed3a1ecf2e04b107b83d850c6cee6642df
Anton Astashov <anton.astashov@gmail.com>
2015-10-15 22:46:53 -0500

    Add index_generator_monit.sh

--- index_generator_monit.sh
+++ index_generator_monit.sh
@@ -5,12 +5,12 @@ DIRROOT=/dartdoc-generator
 
 case $1 in
    start)
-      echo $$ > /var/run/package_generator.pid;
+      echo $$ > /var/run/index_generator.pid;
       mkdir -p $DIRROOT/logs
-      exec 2>&1 dart $DIRROOT/bin/package_generator.dart --dirroot $DIRROOT 1>>$DIRROOT/logs/package_generator_log.txt
+      exec 2>&1 dart $DIRROOT/bin/index_generator.dart --dirroot $DIRROOT 1>>$DIRROOT/logs/index_generator_log.txt
       ;;
     stop)
-      kill `cat /var/run/package_generator.pid` ;;
+      kill `cat /var/run/index_generator.pid` ;;
     *)
       echo "usage: monit.sh {start|stop}" ;;
 esac
